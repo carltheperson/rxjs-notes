930ea5e8fc433f9afdc999aaf860fc558734b9a7
Carl Riis <carl.aagot.riis@gmail.com>
2022-06-08 19:13:01 +0200

    Better async test for observables

--- observable/example.test.ts
+++ observable/example.test.ts
@@ -1,26 +1,30 @@
 import { TestScheduler } from "rxjs/testing";
-import { concatMap, delay, of, throttleTime } from "rxjs";
 import { exampleObservable$ } from "./example";
 
 const testScheduler = new TestScheduler((actual, expected) => {
   expect(actual).toEqual(expected);
 });
 
-it("Observable emits synchronously as expected", () => {
-  testScheduler.run((helpers) => {
-    const { expectObservable } = helpers;
-    const expected = "(a-b-c)";
-    expectObservable(exampleObservable$).toBe(expected);
+describe("Observable", () => {
+  test("Observable emits synchronously as expected", () => {
+    testScheduler.run((helpers) => {
+      const { expectObservable } = helpers;
+      const expected = "(a-b-c)";
+      expectObservable(exampleObservable$).toBe(expected);
+    });
   });
-});
 
-it("Observable emits asynchronously as expected", async () => {
-  let values = ["a", "b", "c", "d"];
-  let i = 0;
-  exampleObservable$.subscribe((value) => {
-    expect(value).toBe(values[i]);
-    i++;
+  test("Observable emits asynchronously as expected", (done) => {
+    let values = ["a", "b", "c", "d"];
+    let i = 0;
+    exampleObservable$.subscribe((value) => {
+      expect(value).toBe(values[i]);
+      i++;
+    });
+    setTimeout(() => expect(i).toBe(values.length - 1), 999);
+    setTimeout(() => {
+      expect(i).toBe(values.length);
+      done();
+    }, 1000);
   });
-  await new Promise((r) => setTimeout(r, 1000));
-  expect(i).toBe(values.length);
 });
