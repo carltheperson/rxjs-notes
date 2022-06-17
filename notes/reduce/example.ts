e320b4586c8fc788934ba0a7c0c5ab90625f68d4
Carl Riis <carl.aagot.riis@gmail.com>
2022-06-17 20:27:38 +0200

    Reduce

---
+++ notes/reduce/example.ts
@@ -0,0 +1,17 @@
+import { Observable, reduce } from "rxjs";
+
+const observable = new Observable<number>((subscriber) => {
+  /*            */ subscriber.next(1);
+  setTimeout(() => subscriber.next(1), 100);
+  setTimeout(() => subscriber.next(1), 200);
+  setTimeout(() => subscriber.next(7), 300);
+  setTimeout(() => subscriber.complete(), 400);
+});
+
+const reducedObservable = observable.pipe(
+  reduce((total, value) => total + value, 0)
+);
+
+reducedObservable.subscribe((value) => {
+  console.log(value);
+});
